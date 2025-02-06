3b12e14bef59eab765c215b1c3e28518656ebc08
Chanran kim <126753671+isliese@users.noreply.github.com>
2025-02-05 18:58:11 -0800

    first work on dot_processor

--- Python Exercise/dot_product.py
+++ Python Exercise/dot_product.py
@@ -1,1 +1,18 @@
 # Isla Kim
+
+import data_loader
+
+# Load the data from Q1.csv and Q2.csv using the functions from data_loader.py
+L1_data = np.array(data_loader.csv_file_loader("L1.csv"), dtype=float)
+Q1_data = np.array(data_loader.csv_file_loader("Q1.csv"), dtype=float)
+
+# Loads the 𝑥 values from “L1.csv” and from “Q1.csv”
+L1_x = []
+for row in L1_data:
+    L1_x.append(row[1])
+
+Q1_x = []
+for row in Q1_data:
+    Q1_x.append(row[1])
+
+    
