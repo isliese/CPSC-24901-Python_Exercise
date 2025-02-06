831fd43ce6b99c91673b50b4dc73ee9b261047f4
Chanran kim <126753671+isliese@users.noreply.github.com>
2025-02-05 18:49:11 -0800

    made code prettier

--- Python Exercise/data_loader.py
+++ Python Exercise/data_loader.py
@@ -17,24 +17,24 @@ def json_file_loader(file_name):
         json_data_array = json_data["linear"]  
     return json_data_array  
 
-# Read and load the data stored in each of the file “L1.csv”, “Q1.csv”, “Q2.csv”
-print('\n"L1.csv": ')
-L1_data = csv_file_loader("L1.csv")
-for row in L1_data:
-    print(row) 
+# Read and load the data stored in each of the file “L1.csv”, “Q1.csv”, “Q2.csv”, “L1.json”
+if __name__ == "__main__": # Process only when this script runs directly
+    print('\n"L1.csv": ')
+    L1_data = csv_file_loader("L1.csv")
+    for row in L1_data:
+        print(row) 
 
-print('\n"Q1.csv": ')
-Q1_data = csv_file_loader("Q1.csv")
-for row in Q1_data:
-    print(row)  
+    print('\n"Q1.csv": ')
+    Q1_data = csv_file_loader("Q1.csv")
+    for row in Q1_data:
+        print(row)  
 
-print('\n"Q2.csv": ')
-Q2_data = csv_file_loader("Q2.csv")
-for row in Q2_data:
-    print(row) 
+    print('\n"Q2.csv": ')
+    Q2_data = csv_file_loader("Q2.csv")
+    for row in Q2_data:
+        print(row) 
 
-# Read and load the data stored in each of the file “L1.json”
-print('\n"L1.json": ')
-L1_json_data = json_file_loader("L1.json")
-for row in L1_json_data:
-    print(row) 
+    print('\n"L1.json": ')
+    L1_json_data = json_file_loader("L1.json")
+    for row in L1_json_data:
+        print(row) 
